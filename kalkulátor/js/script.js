88819761f633053b3ab71cb3a34f05db03032c27
Kenesi Kornél <Ny19KenesiK@szgya.hu>
2022-01-11 13:56:20 +0100

    megoldas

--- kalkulátor/js/script.js
+++ kalkulátor/js/script.js
@@ -1,8 +1,23 @@
 function szamol() {
-    let szam1szoveg =  document.getElementById("szam1").Value;
-    let szam2szoveg = document.getElementById("szam2").Value;
-    console.log(`Első szám :${szam1szoveg}`);
-    console.log(`Második szám ;${szam2szoveg}`);
+    let szam1szoveg =  document.getElementById("szam1").value;
+    let szam2szoveg = document.getElementById("szam2").value;
+    let muv = document.getElementById("muvelet").value;
+    let eredmeny = 0;
+
+    let x = parseInt(szam1szoveg);
+    let y = parseInt(szam2szoveg);
+
+    if (muv === "osszead") {
+        eredmeny = x + y;
+    } else if (muv === "kivon"){
+        eredmeny = x - y;
+    } else if (muv === "oszt"){
+        eredmeny = x / y;
+    } else if (muv === "szoroz"){
+        eredmeny = x * y;
+    }
+
+
+    console.log(eredmeny);
 }
 document.getElementById("szam1").focus();
-szamol();
